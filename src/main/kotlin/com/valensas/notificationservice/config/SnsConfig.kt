6b032cad0a7f40e952de76b36cad0c906558ec2b
ErayOnder <erayonder21@ku.edu.tr>
2024-07-09 15:12:36 +0300

    Added SnsConfig class

---
+++ src/main/kotlin/com/valensas/notificationservice/config/SnsConfig.kt
@@ -0,0 +1,24 @@
+package com.valensas.notificationservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.services.sns.SnsClient
+
+@Configuration
+class SnsConfig(
+    private val awsProperties: AWSProperties
+) {
+    @Bean
+    fun snsClient(): SnsClient {
+        val basicAWSCredentials = AwsBasicCredentials.create(
+            awsProperties.credentials.accessKey,
+            awsProperties.credentials.secretKey
+        )
+        return SnsClient.builder()
+            .credentialsProvider(StaticCredentialsProvider.create(basicAWSCredentials))
+            .region(awsProperties.region.static)
+            .build()
+    }
+}
