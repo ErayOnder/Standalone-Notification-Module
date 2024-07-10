ce908753cf6cbe3f5456106a697ff74a7e368737
Fırat Batar <firat.o.batar@gmail.com>
2024-07-10 10:54:28 +0300

    Add SMTPConfig and SMTPProperties classes and configure application.yaml accordingly
    Configure JavaMailSender for use with SMTP service, overriding AWS' auto configuration.

---
+++ src/main/kotlin/com/valensas/notificationservice/config/SMTPProperties.kt
@@ -0,0 +1,12 @@
+package com.valensas.notificationservice.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "spring.mail")
+data class SMTPProperties(
+    val host: String,
+    val port: Int,
+    val username: String,
+    val password: String,
+    val properties: Map<String, String>
+)
