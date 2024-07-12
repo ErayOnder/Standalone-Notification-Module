641be4712dcff86cdbf99e66b9665967e423739d
ErayOnder <erayonder21@ku.edu.tr>
2024-07-12 10:37:56 +0300

    Added 4 EmailService test concerning email request format

--- src/test/kotlin/com/valensas/notificationservice/EmailServiceTest.kt
+++ src/test/kotlin/com/valensas/notificationservice/EmailServiceTest.kt
@@ -4,7 +4,9 @@ import com.valensas.notificationservice.model.EmailBody
 import com.valensas.notificationservice.model.EmailChannel
 import com.valensas.notificationservice.model.EmailModel
 import com.valensas.notificationservice.service.EmailService
+import jakarta.mail.Message
 import jakarta.mail.internet.MimeMessage
+import jakarta.mail.internet.MimeMultipart
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -12,12 +14,14 @@ import org.mockito.ArgumentCaptor
 import org.mockito.Captor
 import org.mockito.Mock
 import org.mockito.Mockito
+import org.mockito.Mockito.verify
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpStatus
 import org.springframework.mail.MailAuthenticationException
 import org.springframework.mail.MailSendException
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.JavaMailSenderImpl
 import org.springframework.test.context.ActiveProfiles
 import kotlin.test.assertEquals
 
@@ -38,6 +42,8 @@ class EmailServiceTest {
 
     private lateinit var awsEmailModel: EmailModel
     private lateinit var smtpEmailModel: EmailModel
+    private lateinit var awsEmailModelWithoutHTML: EmailModel
+    private lateinit var smtpEmailModelWithoutHTML: EmailModel
     private lateinit var emailService: EmailService
 
     @Captor
@@ -70,6 +76,28 @@ class EmailServiceTest {
                 EmailBody(
                     "Plain message",
                     "<h1>HTML message</h1>",
+                ),
+                "Test subject",
+                EmailChannel.SMTP,
+            )
+
+        awsEmailModelWithoutHTML =
+            EmailModel(
+                "test@example.com",
+                EmailBody(
+                    "Plain message",
+                    null,
+                ),
+                "Test subject",
+                EmailChannel.AWS,
+            )
+
+        smtpEmailModelWithoutHTML =
+            EmailModel(
+                "test@example.com",
+                EmailBody(
+                    "Plain message",
+                    null,
                 ),
                 "Test subject",
                 EmailChannel.SMTP,
@@ -140,5 +168,98 @@ class EmailServiceTest {
 
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.statusCode)
         assertEquals("Mail failed to sent to ${smtpEmailModel.receiver} with subject ${smtpEmailModel.subject}", response.body)
+    }
+
+    @Test
+    fun `send mime message verification for aws channel`() {
+        Mockito.`when`(sesMailSender.createMimeMessage()).thenReturn(JavaMailSenderImpl().createMimeMessage())
+        emailService.send(awsEmailModel)
+        verify(sesMailSender).send(mimeMessageCaptor.capture())
+        val mimeMessage = mimeMessageCaptor.value
+
+        assertEquals(awsSender, mimeMessage.from[0].toString())
+        assertEquals(awsEmailModel.subject, mimeMessage.subject)
+        assertEquals(awsEmailModel.receiver, mimeMessage.getRecipients(Message.RecipientType.TO)[0].toString())
+
+        val plainTextContent = getContentFromMultipart(mimeMessage.content as MimeMultipart, "text/plain")
+        val htmlTextContent = getContentFromMultipart(mimeMessage.content as MimeMultipart, "text/html")
+        assertEquals(awsEmailModel.body.plainMessage, plainTextContent)
+        assertEquals(awsEmailModel.body.htmlMessage, htmlTextContent)
+    }
+
+    @Test
+    fun `send mime message verification for smtp channel`() {
+        Mockito.`when`(smtpMailSender.createMimeMessage()).thenReturn(JavaMailSenderImpl().createMimeMessage())
+        emailService.send(smtpEmailModel)
+        verify(smtpMailSender).send(mimeMessageCaptor.capture())
+        val mimeMessage = mimeMessageCaptor.value
+
+        assertEquals(smtpSender, mimeMessage.from[0].toString())
+        assertEquals(smtpEmailModel.subject, mimeMessage.subject)
+        assertEquals(smtpEmailModel.receiver, mimeMessage.getRecipients(Message.RecipientType.TO)[0].toString())
+
+        val plainTextContent = getContentFromMultipart(mimeMessage.content as MimeMultipart, "text/plain")
+        val htmlTextContent = getContentFromMultipart(mimeMessage.content as MimeMultipart, "text/html")
+        assertEquals(smtpEmailModel.body.plainMessage, plainTextContent)
+        assertEquals(smtpEmailModel.body.htmlMessage, htmlTextContent)
+    }
+
+    @Test
+    fun `send mime message verification with html null for aws channel`() {
+        Mockito.`when`(sesMailSender.createMimeMessage()).thenReturn(JavaMailSenderImpl().createMimeMessage())
+        emailService.send(awsEmailModelWithoutHTML)
+        verify(sesMailSender).send(mimeMessageCaptor.capture())
+        val mimeMessage = mimeMessageCaptor.value
+
+        assertEquals(awsSender, mimeMessage.from[0].toString())
+        assertEquals(awsEmailModelWithoutHTML.subject, mimeMessage.subject)
+        assertEquals(awsEmailModelWithoutHTML.receiver, mimeMessage.getRecipients(Message.RecipientType.TO)[0].toString())
+
+        val plainTextContent = getContentFromMultipart(mimeMessage.content as MimeMultipart, "text/plain")
+        val htmlTextContent = getContentFromMultipart(mimeMessage.content as MimeMultipart, "text/html")
+        assertEquals(awsEmailModelWithoutHTML.body.plainMessage, plainTextContent)
+        assertEquals(awsEmailModelWithoutHTML.body.htmlMessage, htmlTextContent)
+    }
+
+    @Test
+    fun `send mime message verification with html null for smtp channel`() {
+        Mockito.`when`(smtpMailSender.createMimeMessage()).thenReturn(JavaMailSenderImpl().createMimeMessage())
+        emailService.send(smtpEmailModelWithoutHTML)
+        verify(smtpMailSender).send(mimeMessageCaptor.capture())
+        val mimeMessage = mimeMessageCaptor.value
+
+        assertEquals(smtpSender, mimeMessage.from[0].toString())
+        assertEquals(smtpEmailModelWithoutHTML.subject, mimeMessage.subject)
+        assertEquals(smtpEmailModelWithoutHTML.receiver, mimeMessage.getRecipients(Message.RecipientType.TO)[0].toString())
+
+        val plainTextContent = getContentFromMultipart(mimeMessage.content as MimeMultipart, "text/plain")
+        val htmlTextContent = getContentFromMultipart(mimeMessage.content as MimeMultipart, "text/html")
+        assertEquals(smtpEmailModelWithoutHTML.body.plainMessage, plainTextContent)
+        assertEquals(smtpEmailModelWithoutHTML.body.htmlMessage, htmlTextContent)
+    }
+
+    fun getContentFromMultipart(
+        mimeMultipart: MimeMultipart,
+        type: String,
+    ): String? {
+        val typeIdx =
+            when (type) {
+                "text/plain" -> 0
+                "text/html" -> 1
+                else -> throw RuntimeException("Invalid type")
+            }
+
+        for (i in 0 until mimeMultipart.count) {
+            val bodyPart = mimeMultipart.getBodyPart(i)
+            when (bodyPart.content) {
+                is MimeMultipart -> return getContentFromMultipart(bodyPart.content as MimeMultipart, type)
+                is String -> {
+                    if (i == typeIdx) {
+                        return bodyPart.content as String
+                    }
+                }
+            }
+        }
+        return null
     }
 }
