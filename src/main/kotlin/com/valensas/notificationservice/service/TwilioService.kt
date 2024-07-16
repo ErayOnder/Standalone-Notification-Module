45e081a2f632d9d6df4b06e21091c1bb70c69204
Fırat Batar <firat.o.batar@gmail.com>
2024-07-16 15:19:36 +0300

    Add TwilioService

---
+++ src/main/kotlin/com/valensas/notificationservice/service/TwilioService.kt
@@ -0,0 +1,35 @@
+package com.valensas.notificationservice.service
+
+import com.twilio.exception.ApiException
+import com.twilio.rest.api.v2010.account.Message
+import com.twilio.type.PhoneNumber
+import com.valensas.notificationservice.config.TwilioProperties
+import com.valensas.notificationservice.model.SmsModel
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+
+@Service("smsService")
+@EnableConfigurationProperties(TwilioProperties::class)
+class TwilioService(
+    @Value("\${twilio.from-phone-number}")
+    private val sender: String,
+) : SmsService {
+    override fun send(smsModel: SmsModel): ResponseEntity<String> {
+        try {
+            val message =
+                Message.creator(
+                    PhoneNumber(smsModel.receiver),
+                    PhoneNumber(sender),
+                    smsModel.body,
+                ).create()
+            return ResponseEntity.ok(
+                "SMS sent successfully to ${smsModel.receiver}",
+            )
+        } catch (e: ApiException) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.message)
+        }
+    }
+}
