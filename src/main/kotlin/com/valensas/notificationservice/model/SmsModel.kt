3a7ab76220c1b3e1027f72a04f9e3afdb0987903
ErayOnder <erayonder21@ku.edu.tr>
2024-07-16 14:20:09 +0300

    Applied formating to SmsModel class

--- src/main/kotlin/com/valensas/notificationservice/model/SmsModel.kt
+++ src/main/kotlin/com/valensas/notificationservice/model/SmsModel.kt
@@ -3,12 +3,12 @@ package com.valensas.notificationservice.model
 data class SmsModel(
     val receiver: String,
     val body: String,
-    val type: String
+    val type: String,
 ) {
     init {
         require(
             type.lowercase() == "promotional" ||
-            type.lowercase() == "transactional"
+                type.lowercase() == "transactional",
         )
     }
 }
