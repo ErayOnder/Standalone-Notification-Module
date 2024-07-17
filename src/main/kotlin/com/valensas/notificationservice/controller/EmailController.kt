aa6cc97ca94b450498b1a8a3f07cf7e470509f7e
Fırat Batar <firat.o.batar@gmail.com>
2024-07-17 10:44:12 +0300

    Change conditionals on service and controller creations

--- src/main/kotlin/com/valensas/notificationservice/controller/EmailController.kt
+++ src/main/kotlin/com/valensas/notificationservice/controller/EmailController.kt
@@ -2,14 +2,14 @@ package com.valensas.notificationservice.controller
 
 import com.valensas.notificationservice.model.EmailModel
 import com.valensas.notificationservice.service.EmailService
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@ConditionalOnProperty("notification.email.service")
+@ConditionalOnExpression("'\${notification.email.service}' == 'smtp' || '\${notification.email.service}' == 'ses'")
 class EmailController(
     private val emailService: EmailService,
 ) {
--- src/main/kotlin/com/valensas/notificationservice/controller/SmsController.kt
+++ src/main/kotlin/com/valensas/notificationservice/controller/SmsController.kt
@@ -3,14 +3,14 @@ package com.valensas.notificationservice.controller
 import com.valensas.notificationservice.model.SmsModel
 import com.valensas.notificationservice.service.SmsService
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@ConditionalOnProperty("notification.sms.service")
+@ConditionalOnExpression("'\${notification.sms.service}' == 'twilio' || '\${notification.sms.service}' == 'sns'")
 class SmsController(
     @Qualifier("smsService")
     private val smsService: SmsService,
