6253542763a206404c18ccdcd9913053a22c71e4
Fırat Batar <firat.o.batar@gmail.com>
2024-07-18 11:30:22 +0300

    Update conditional on SmsService

--- src/main/kotlin/com/valensas/notificationservice/controller/SmsController.kt
+++ src/main/kotlin/com/valensas/notificationservice/controller/SmsController.kt
@@ -3,14 +3,14 @@ package com.valensas.notificationservice.controller
 import com.valensas.notificationservice.model.SmsModel
 import com.valensas.notificationservice.service.SmsService
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@ConditionalOnExpression("'\${notification.sms.service}' == 'twilio' || '\${notification.sms.service}' == 'sns'")
+@ConditionalOnBean(SmsService::class)
 class SmsController(
     @Qualifier("smsService")
     private val smsService: SmsService,
