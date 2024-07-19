f624e2c46d2716155c398c319d2169dc8953f2eb
ErayOnder <erayonder21@ku.edu.tr>
2024-07-19 11:26:35 +0300

    Implement phone number generator function

--- src/main/kotlin/com/valensas/notificationservice/model/SmsModel.kt
+++ src/main/kotlin/com/valensas/notificationservice/model/SmsModel.kt
@@ -28,3 +28,12 @@ fun validatePhoneNumber(phoneNumber: String) {
         throw IllegalArgumentException("Invalid phone number.")
     }
 }
+
+fun generatePhoneNumber(isValid: Boolean, regionCode: String, format: PhoneNumberUtil.PhoneNumberFormat): String {
+    val phoneNumberUtil = PhoneNumberUtil.getInstance()
+    val phoneNumber = when (isValid) {
+        true -> phoneNumberUtil.getExampleNumber(regionCode)
+        false -> phoneNumberUtil.getInvalidExampleNumber(regionCode)
+    }
+    return phoneNumberUtil.format(phoneNumber, format)
+}
